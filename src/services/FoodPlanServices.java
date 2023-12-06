df6723f5635f14c3adea4be0b9e69df67aa4d077
Antonio <antoniolamv91@gmail.com>
2023-12-05 23:43:39 -0500

    todo esto por culpa de tu rama yankiel

--- src/services/FoodPlanServices.java
+++ src/services/FoodPlanServices.java
@@ -1,5 +1,79 @@
 package services;
 
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import dto.FoodPlanDTO;
+import dto.UserDTO;
+
 public class FoodPlanServices {
+	
+	public void insertFoodPlan(String typeOfFoodPlan) 
+			throws SQLException, ClassNotFoundException{
+		String query = "SELECT food_plan_insert(?)";
+		java.sql.Connection connection = ServicesLocator.getConnection();
+		PreparedStatement preparedStatement = connection.prepareStatement(query);
+		preparedStatement.setString(1, typeOfFoodPlan);
+		preparedStatement.execute();
+		preparedStatement.close();
+		connection.close();
+	}
+	
+	public void deleteFoodPlan(int foodPlanCode) throws SQLException, ClassNotFoundException{
+		String query = "SELECT food_plan_delete(?)";
+		java.sql.Connection connection = ServicesLocator.getConnection();
+		PreparedStatement preparedStatement = connection.prepareStatement(query);
+		preparedStatement.setInt(1, foodPlanCode);
+		preparedStatement.execute();
+		preparedStatement.close();
+		connection.close();
+	}
+	
+	public void updateFoodPlan(int foodPlaceCode, String typeOfFoodPlan) 
+			throws SQLException, ClassNotFoundException{
+		String query = "SELECT food_plan_update(?,?)";
+		java.sql.Connection connection = ServicesLocator.getConnection();
+		PreparedStatement preparedStatement = connection.prepareStatement(query);
+		preparedStatement.setInt(1, foodPlaceCode);
+		preparedStatement.setString(2, typeOfFoodPlan);
+		preparedStatement.execute();
+		preparedStatement.close();
+		connection.close();
+	}
+	
+	public FoodPlanDTO findFoodPlan(int foodPlanCode) throws SQLException, ClassNotFoundException{
+		java.sql.Connection connection = ServicesLocator.getConnection();
+		Statement statement = connection.createStatement (ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY); 
+		String query = "SELECT * FROM food_plan WHERE food_plan.food_plan_code = '"+foodPlanCode+"'"; 
+		ResultSet rs = statement.executeQuery(query);
+		rs.first();
+		FoodPlanDTO foodPlan = new FoodPlanDTO(rs.getInt(1), rs.getString(2));
+		rs.close();
+		statement.close();
+		connection.close();
+		return foodPlan;
+	}
+	
+	public ArrayList<FoodPlanDTO> selectAllFoddPlans() throws SQLException, ClassNotFoundException{
+		ArrayList<FoodPlanDTO> foodPlans = new ArrayList<FoodPlanDTO>();
+		String function = "{?= call select_all_food_plan()}";
+		java.sql.Connection connection = ServicesLocator.getConnection();
+		connection.setAutoCommit(false);
+		CallableStatement preparedFunction = connection.prepareCall(function);
+		preparedFunction.registerOutParameter(1, java.sql.Types.OTHER);
+		preparedFunction.execute();
+		ResultSet rs = (ResultSet) preparedFunction.getObject(1);
+		while (rs.next()){
+			foodPlans.add(new FoodPlanDTO(rs.getInt(1), rs.getString(2)));
+		}
+		rs.close();
+		preparedFunction.close();
+		connection.close();
+		return foodPlans;
+	}
 
 }
--- src/services/RoomServices.java
+++ src/services/RoomServices.java
@@ -1,5 +1,83 @@
 package services;
 
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import dto.RoomDTO;
+import dto.UserDTO;
+
 public class RoomServices {
+	
+	public void insertRoom(String roomType, double surchargeRoom, int foodPlanCode) 
+			throws SQLException, ClassNotFoundException{
+		String query = "SELECT room_insert(?,?,?)";
+		java.sql.Connection connection = ServicesLocator.getConnection();
+		PreparedStatement preparedStatement = connection.prepareStatement(query);
+		preparedStatement.setString(1, roomType);
+		preparedStatement.setDouble(2, surchargeRoom);
+		preparedStatement.setInt(3, foodPlanCode);
+		preparedStatement.execute();
+		preparedStatement.close();
+		connection.close();
+	}
+	
+	public void deleteRoom(int RoomCode) throws SQLException, ClassNotFoundException{
+		String query = "SELECT room_delete(?)";
+		java.sql.Connection connection = ServicesLocator.getConnection();
+		PreparedStatement preparedStatement = connection.prepareStatement(query);
+		preparedStatement.setInt(1, RoomCode);
+		preparedStatement.execute();
+		preparedStatement.close();
+		connection.close();
+	}
+	
+	public void updateRoom(int roomCode, String roomType, double surchargeRoom, int foodPlanCode) 
+			throws SQLException, ClassNotFoundException{
+		String query = "SELECT room_update(?,?,?,?)";
+		java.sql.Connection connection = ServicesLocator.getConnection();
+		PreparedStatement preparedStatement = connection.prepareStatement(query);
+		preparedStatement.setInt(1, roomCode);
+		preparedStatement.setString(2, roomType);
+		preparedStatement.setDouble(3, surchargeRoom);
+		preparedStatement.setInt(4, foodPlanCode);
+		preparedStatement.execute();
+		preparedStatement.close();
+		connection.close();
+	}
+	
+	public RoomDTO findRoom(int roomCode) throws SQLException, ClassNotFoundException{
+		java.sql.Connection connection = ServicesLocator.getConnection();
+		Statement statement = connection.createStatement (ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY); 
+		String query = "SELECT * FROM room WHERE room.room_code = '"+roomCode+"'"; 
+		ResultSet rs = statement.executeQuery(query);
+		rs.first();
+		RoomDTO room = new RoomDTO(rs.getInt(1), rs.getString(2), rs.getDouble(3), rs.getInt(4));
+		rs.close();
+		statement.close();
+		connection.close();
+		return room;
+	}
+	
+	public ArrayList<RoomDTO> selectAllRooms() throws SQLException, ClassNotFoundException{
+		ArrayList<RoomDTO> rooms = new ArrayList<RoomDTO>();
+		String function = "{?= call select_all_room()}";
+		java.sql.Connection connection = ServicesLocator.getConnection();
+		connection.setAutoCommit(false);
+		CallableStatement preparedFunction = connection.prepareCall(function);
+		preparedFunction.registerOutParameter(1, java.sql.Types.OTHER);
+		preparedFunction.execute();
+		ResultSet rs = (ResultSet) preparedFunction.getObject(1);
+		while (rs.next()){
+			rooms.add(new RoomDTO(rs.getInt(1), rs.getString(2), rs.getDouble(3), rs.getInt(4)));
+		}
+		rs.close();
+		preparedFunction.close();
+		connection.close();
+		return rooms;
+	}
 
 }
